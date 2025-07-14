c85890587dca021f2b6bc15710e24bcd257d4201
TheBaytur <bayturcodding@gmail.com>
2025-07-14 14:17:32 +0800

    Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:provider/provider.dart';
 
@@ -22,6 +25,9 @@ class MyApp extends StatelessWidget {
       home: MultiProvider(providers: [
         ChangeNotifierProvider<CountProvider>.value(value: CountProvider()),
         // Add other providers here if needed
+        FutureProvider(create: (_) async => 
+        UserProvider().loadUserData(),
+        initialData: [],),
       ],
       child: DefaultTabController(
         length: 3,
@@ -43,8 +49,8 @@ class MyApp extends StatelessWidget {
           body: const TabBarView(
             children: <Widget>[
               MyCountPage(),
-              // MyUserPage(),
-              // MyEventPage(),
+              MyUserPage(),
+              MyEventPage(),
             ],
           ),
         ),
@@ -57,6 +63,8 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
 
 class MyCountPage extends StatelessWidget {
   const MyCountPage({super.key});
@@ -122,6 +130,53 @@ class MyCountPage extends StatelessWidget {
   }  
 }
 
+class MyUserPage extends StatelessWidget {
+  const MyUserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(padding: EdgeInsets.all(10.0),
+        child: Text('User Page', style: TextStyle(fontSize: 14)),
+        ),
+        Consumer<List<User>>(
+          builder: (context, users, _) {
+            return Expanded(
+              child: users == null
+              ? Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 50,
+                    color: Colors.blueAccent,
+                    child: Center(
+                      child: Text('User ${users[index].name} ${users[index].nickname} ${users[index].email}', style: TextStyle(color: Colors.white, fontSize: 18)),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
+        
+      ],
+    );
+  }
+}
+
+class MyEventPage extends StatelessWidget {
+  const MyEventPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Event Page', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
+
 class CountProvider extends ChangeNotifier {
   int count = 0;
 
@@ -140,6 +195,47 @@ class CountProvider extends ChangeNotifier {
   }
 }
 
+class UserProvider{
+  final String _dataPath = 'assets/users.json';
 
+  late List<User> users;
+
+  Future<String> loadAsset () async {
+    return await Future.delayed( Duration(seconds: 2), () async{
+      return await rootBundle.loadString(_dataPath);
+    });
+  }
+  Future<List<User>> loadUserData() async {
+    var dataString = await loadAsset();
+    Map<String, dynamic> jsonUserData = jsonDecode(dataString);
+    users = UserList.fromJson(jsonUserData['users']).users;
+    print('Loaded users' + jsonEncode(users));
+    return users;
+  }
+}
+
+class User {
+  final String name;
+  final String nickname;
+  final String email;
+
+  User({required this.name, required this.nickname, required this.email});
+
+  User.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        nickname = json['nickname'],
+        email = json['email'];
+}
+
+class UserList {
+  final List<User> users;
+
+  UserList({required this.users});
+
+UserList.fromJson(List<dynamic> usersJson) 
+       :   users = usersJson.map((user) => User.fromJson(user)).toList();
+
+  
+}
 
 
